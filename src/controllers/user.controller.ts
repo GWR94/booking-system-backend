9c5a6debfb70c23ff3e942738ef971c1e5b73364
James Gower <jgower.dev@gmail.com>
2026-01-09 01:04:06 +0000

    fix(auth): re-enable cookie domain for custom domain

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -14,11 +14,16 @@ const isProduction =
   process.env.NODE_ENV === "production" ||
   process.env.FRONT_END?.startsWith("https");
 
+const domain = isProduction
+  ? new URL(process.env.FRONT_END!).hostname.replace(/^[^.]+\./g, ".")
+  : undefined;
+
 const accessTokenConfig: CookieOptions = {
   httpOnly: true,
   secure: isProduction,
   sameSite: isProduction ? "none" : "lax",
   maxAge: 15 * 60 * 1000, // 15 minutes
+  domain,
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
@@ -28,6 +33,7 @@ const refreshTokenConfig: CookieOptions = {
   secure: isProduction,
   sameSite: isProduction ? "none" : "lax",
   path: "/api/user/refresh",
+  domain,
 };
 
 // Register a new user
